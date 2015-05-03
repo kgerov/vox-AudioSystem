a6dad06b269167b2bcdbe23b78ac8f96e66bad71
Konstantin Gerov <kgerov@Konstantins-MacBook-Pro.local>
2015-05-03 13:13:18 +0300

    router extended

--- voxFramework/Routers/DefaultRouter.php
+++ voxFramework/Routers/DefaultRouter.php
@@ -5,6 +5,24 @@ namespace Vox\Routers;
 class DefaultRouter {
 	public function parse() {
 		$_uri = substr(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PHP_SELF']), 1);
-		echo $_uri;
+		$controller = null;
+		$method = null;
+		$_params = explode('/', $_uri);
+
+		if ($_params[0]) {
+			$controller = ucfirst($_params[0]);
+
+			if ($_params[1]) {
+				$method = $_params[1];
+				unset($_params[0], $_params[1]);
+			} else {
+				$method = 'index';
+			}
+		} else {
+			$controller = 'index';
+			$method = 'index';
+		}
+
+		echo $controller . '<br>' . $method;
 	}
 }
