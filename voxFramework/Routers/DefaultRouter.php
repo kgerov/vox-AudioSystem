8156934e13df5a24c7c084956d449de28aee2366
Konstantin Gerov <kgerov@Konstantins-MacBook-Pro.local>
2015-05-03 14:20:33 +0300

    router finished

--- voxFramework/Routers/DefaultRouter.php
+++ voxFramework/Routers/DefaultRouter.php
@@ -3,26 +3,34 @@
 namespace Vox\Routers;
 
 class DefaultRouter {
+	private $controller = null;
+	private $method = null;
+	private $params = array();
+
 	public function parse() {
 		$_uri = substr($_SERVER['PHP_SELF'], strlen($_SERVER['SCRIPT_NAME']) + 1);
-		$controller = null;
-		$method = null;
 		$_params = explode('/', $_uri);
 
 		if ($_params[0]) {
-			$controller = ucfirst($_params[0]);
+			$this->controller = ucfirst($_params[0]);
 
 			if ($_params[1]) {
-				$method = $_params[1];
+				$this->method = $_params[1];
 				unset($_params[0], $_params[1]);
-			} else {
-				$method = 'index';
-			}
-		} else {
-			$controller = 'index';
-			$method = 'index';
-		}
+				$this->params = array_values($_params);
+			} 
+		} 
+	}
 
-		echo $controller . '<br>' . $method;
+	public function getController() {
+		return $this->controller;
+	}
+
+	public function getMethod() {
+		return $this->method;
+	}
+
+	public function getGet() {
+		return $this->params;
 	}
 }
