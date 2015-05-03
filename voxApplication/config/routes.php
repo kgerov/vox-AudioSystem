5cbda9d9c1a2306738857d62d2cd7a1ed5915a0f
Konstantin Gerov <kgerov@Konstantins-MacBook-Pro.local>
2015-05-03 20:31:28 +0300

    front controller upgraded

--- voxApplication/config/routes.php
+++ voxApplication/config/routes.php
@@ -1,8 +1,10 @@
 <?php
-$cnf['admin']['namespace'] = 'Controllers/Admin';
-$cnf['administration']['namespace'] = 'Controllers/Admin';
+$cnf['admin']['namespace'] = 'Controllers/Admin1';
+$cnf['administration']['namespace'] = 'Controllers/Admin2';
 $cnf['administration']['controllers']['index'] = 'test';
 $cnf['administration']['controllers']['new'] = 'create';
 $cnf['*']['namespace'] = 'Controllers';
 
 return $cnf;
+
+//$cnf['admin/users']['namespace'] = 'Controllers/Admin1';
--- voxFramework/FrontController.php
+++ voxFramework/FrontController.php
@@ -16,11 +16,17 @@ class FrontController {
 		$a = new \Vox\Routers\DefaultRouter();
 		$_uri = $a->getURI();
 		$routes = \Vox\App::getInstance()->getConfig()->routes;
+		$_rc = null;
 
 		if (is_array($routes) && count($routes) > 0) {
 			foreach ($routes as $k => $v) {
-				if (strpos($_uri, $k) === 0 && $v['namespace']) {
+				if (stripos($_uri, $k) === 0 && 
+					($_uri == $k || stripos($_uri, $k.'/') === 0) && 
+					$v['namespace']) {
+
 					$this->ns = $v['namespace'];
+					$_uri = substr($_uri, strlen($k) + 1);
+					$_rc = $v;
 					break;
 				}
 			}
@@ -30,11 +36,31 @@ class FrontController {
 
 		if ($this->ns == null && $routes['*']['namespace']) {
 			$this->ns = $routes['*']['namespace'];
+			$_rc = $routes['*'];
 		} else if($this->ns == null && !$routes['*']['namespace']) {
 			throw new \Exception('Default route missing', 500);
 		}
 
-		echo $this->ns;
+		$_params = explode('/', $_uri);
+
+		if ($_params[0]) {
+			$this->controller = $_params[0];
+
+			if ($_params[1]) {
+				$this->method = $_params[1];
+			} else {
+				$this->method = $this->getDefaultMethod();
+			}
+		} else {
+			$this->controller = $this->getDefaultController();
+			$this->method = $this->getDefaultMethod();
+		}
+
+		if (is_array($_rc) && $_rc['controllers'] && $_rc['controllers'][$this->controller]) {
+			$this->controller = $_rc['controllers'][$this->controller];
+		}
+
+		echo $this->controller;
 	}
 
 	/**
